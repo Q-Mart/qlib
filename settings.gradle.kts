81693638c061da7535fa02adc1f8c0bdef7ca02b
Qumarth Jash <qumarthjash@gmail.com>
2025-03-10 11:23:59 +0000

    Natural line generator (v basic)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "qlib"
-
+include("src:main:test")
+findProject(":src:main:test")?.name = "test"
--- src/main/java/uk/qumarth/qlib/Drawing.java
+++ src/main/java/uk/qumarth/qlib/Drawing.java
@@ -4,7 +4,9 @@ import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PVector;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -28,5 +30,38 @@ public class Drawing {
                 sketch.vertex(v1.x, v1.y);
             }
         });
+    }
+
+    public static void drawNaturalLine(PVector start, PVector end, Random r, PApplet sketch) {
+        // Add a random slant
+        PVector slantedStart = addNoiseToPVector(start, r, sketch);
+        PVector slantedEnd = addNoiseToPVector(end, r, sketch);
+
+        // Interpolate points and add wobble
+        List<PVector> points = new ArrayList<>();
+        points.add(slantedStart);
+        for (float interpolationAmount = 0.001f; interpolationAmount < 1f; interpolationAmount += 0.001f) {
+            PVector nextPoint = new PVector(
+                    PApplet.lerp(slantedStart.x, slantedEnd.x, interpolationAmount),
+                    PApplet.lerp(slantedStart.y, slantedEnd.y, interpolationAmount)
+            );
+
+            points.add(addNoiseToPVector(nextPoint, r, sketch));
+        }
+
+        points.add(slantedEnd);
+
+        sketch.beginShape();
+        for (PVector point: points) {
+            sketch.vertex(point.x, point.y);
+        }
+        sketch.endShape();
+    }
+
+    private static PVector addNoiseToPVector(PVector v, Random r, PApplet sketch) {
+        return new PVector(
+                v.x + RandomUtils.randomGaussian(sketch.width/100000f, sketch.width/1000f, r),
+                v.y + RandomUtils.randomGaussian(sketch.height/100000f, sketch.height/1000f, r)
+        );
     }
 }
